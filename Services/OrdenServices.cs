a40378020ed7b25aaaaf28c119c6e5b46c8ee0b4
Brian Dariel <20198375@itla.edu.do>
2021-08-16 19:18:27 -0400

    Creacion de servicios

---
+++ Services/OrdenServices.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Avocado_Market.Data;
+
+namespace Avocado_Market.Services
+{
+    public interface IOrdenServices
+    {
+        Task<List<Orden>> Get();
+        Task<Orden> Get(int id);
+        Task<Orden> Add(Orden cate);
+        Task<Orden> Update(Orden cate);
+        Task<Orden> Delete(int id);
+    }
+    public class OrdenServices : IOrdenServices
+    {
+        private readonly UsuarioContext _context;
+        public OrdenServices(UsuarioContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<Orden>> Get()
+        {
+            return await _context.Ordenes.ToListAsync();
+        }
+
+        public async Task<Orden> Get(int id)
+        {
+            var OrdenExacta = await _context.Ordenes.FindAsync(id);
+            return OrdenExacta;
+        }
+        public async Task<Orden> Add(Orden cate)
+        {
+            _context.Ordenes.Add(cate);
+            await _context.SaveChangesAsync();
+            return cate;
+        }
+        public async Task<Orden> Delete(int id)
+        {
+            var cate = await _context.Ordenes.FindAsync(id);
+            _context.Ordenes.Remove(cate);
+            await _context.SaveChangesAsync();
+            return cate;
+        }
+
+
+
+        public async Task<Orden> Update(Orden cate)
+        {
+            _context.Update(cate);
+            await _context.SaveChangesAsync();
+            return cate;
+        }
+    }
+}
